9356b72d4d0f4a521c4e704a792780b7a482ca0c
Deep Tensor <subhanshuverma24@gmail.com>
2026-07-01 11:09:56 +0530

    Model Hyperparameter tunning

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import dill as pickle
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -18,11 +19,16 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models,params,cv=3,n_jobs=3,verbose=True):
     try:
         model_report = {}
         for i in range(len(models)):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv=cv, n_jobs=n_jobs,verbose=verbose,refit=True)
+            gs.fit(X_train, y_train)
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
